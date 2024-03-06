03e6177d9613e9378abdd0c604ae5837d6492af3
André Amado <andre.amado@pm.me>
2024-03-06 18:35:11 +0000

    added member visualization

---
+++ member.py
@@ -0,0 +1,32 @@
+from flask import Blueprint, render_template
+
+from .db import db_session
+from .auth import login_required
+from .models import Member
+
+from sqlalchemy import select
+
+bp = Blueprint('associado', __name__, url_prefix='/associado')
+
+@bp.route('/lista/')
+# @login_required
+def list_members():
+    members = db_session.execute(select(Member.id, Member.number, Member.given_name, Member.family_name)).fetchall()
+
+    return render_template(
+        'members/list.html',
+        members=members,
+        lang='pt'
+    )
+
+@bp.route('/<int:id>')
+# @login_required
+def show_member(id: int):
+    member = db_session.execute(select(Member).where(Member.id == id)).fetchone()
+
+    return render_template(
+        'members/show.html',
+        member=member[0],
+        lang='pt'
+    )
+
