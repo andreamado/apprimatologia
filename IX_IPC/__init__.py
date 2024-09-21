48583abfa4c8ef44a8291f705da853a7cabc6fc6
André Amado <andre.amado@pm.me>
2024-09-21 15:51:46 +0100

    closing abstract submissions

--- IX_IPC/__init__.py
+++ IX_IPC/__init__.py
@@ -29,7 +29,7 @@ from reportlab.lib.styles import getSampleStyleSheet, ParagraphStyle
 bp = Blueprint('IX_IPC', __name__, template_folder='templates')
 
 
-abstract_submission_open = datetime.datetime.now() < datetime.datetime(2024, 9, 21, 4)
+abstract_submission_open = False
 registration_open = False
 
 def login_IXIPC_required(view):
